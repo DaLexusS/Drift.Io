a1ac4ad75afb647a04f74f7714b88cad03924e14
DaLexusS <150054010+DaLexusS@users.noreply.github.com>
2025-01-19 17:41:08 +0200

    pause ui
    Pause temp ui

--- Assets/Scripts/UI/PauseButton.cs
+++ Assets/Scripts/UI/PauseButton.cs
@@ -6,16 +6,19 @@ using UnityEngine.Events;
 public class PauseButton : MonoBehaviour
 {
     public bool IsPaused = false;
+    [SerializeField] public GameObject panel;
     public void PausePressed()
     {
         if (IsPaused)
         {
             IsPaused = false;
+            panel.SetActive(false);
             Time.timeScale = 1f;
         }
         else
         {
             IsPaused = true;
+            panel.SetActive(true);
             Time.timeScale = 0f;
         }
     }
